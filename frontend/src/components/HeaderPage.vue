3f745e19a2d5533ae5d699c51d989430ba556907
o-kinnen <oliviakinnen@gmail.com>
2024-10-28 20:51:55 +0100

    Création du header avec lien vers la page d'inscription (#5)

---
+++ frontend/src/components/HeaderPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light background-nav">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">Accueil</router-link>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link to="/inscription" class="nav-link">Créer un compte</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    </header>
+</template>
+  
+<script>
+export default {
+    name: 'HeaderPage',
+};
+</script>

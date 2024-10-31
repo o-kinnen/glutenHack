ebac6a93affd8ee59d302f1451e4bca1bd2a6cab
o-kinnen <oliviakinnen@gmail.com>
2024-10-31 19:14:21 +0100

    Amélioration du style

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   { path: '/', name: 'HomePage', component: () => import('@/views/HomePage.vue') },
-  { path: '/inscription', name: 'SignupPage', component: () => import('@/views/SignupPage.vue') }
+  { path: '/inscription', name: 'SignupPage', component: () => import('@/views/SignupPage.vue') },
+  { path: '/conditions', name: 'TermsOfService', component: () => import('@/views/TermsOfService.vue') }
 ]
 
 const router = createRouter({
